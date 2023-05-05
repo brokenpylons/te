94e68c771e53727022c4606349325d7128cfa23a
Žiga Leber <zigaleber7@gmail.com>
2023-05-05 20:35:10 +0200

    Add TN constructions

--- bin/main.ml
+++ bin/main.ml
@@ -8,18 +8,23 @@ let set x = R.lits Tn.Lits.(codes (T.Codes.of_string x))
 
 let var x = R.lits Tn.Lits.(var x)
 
+let eof = R.lits Tn.Lits.eof
+
 let () = 
-  let Vector.[s'; s; b] = T.Var.make ~supply:T.Var.supply Vector.["S'"; "S"; "B"] in
-  let ps = Tn.Production.[
-      make s' R.(var s * plus (set "$"));
-      make s R.((set "a" + var b) * (star (set "c")));
-      make b R.((set "d" * var b) + null); 
+  let Vector.[s'; s; a; b; c; u] = T.Var.make ~supply:T.Var.supply Vector.["S'"; "S"; "A"; "B"; "C"; "_"] in
+  let ps = List.to_seq @@ Tn.Production.[
+      make (u, s') R.(var s * plus eof);
+      make (u, s) R.(star (var a + var b + var c));
+      make (u, a) R.(plus (set "a" * set "b") * set "a");
+      make (u, b) R.(set "a" * star (set "b" * set "a"));
+      make (u, c) (set "a");
+
+      (*make (u, s) R.((set "a" * var b) * (star (set "c")) + (var b * set "g"));*)
+      (*make (u, b) R.((var b * set "d" ) + null);*)
   ]
   in
   print_endline @@
   Dot.string_of_graph @@
-  Tn.M.to_dot ~string_of_labels:(fun _ -> "") ~string_of_lits:(fun x -> Fmt.to_to_string Tn.Lits.pp x) (Tn.convert ~supply:T.State.supply (Tn.Production.make s R.(((set "d" + var b) + null))));
-  print_endline @@
-  Dot.string_of_graph @@
-  Tn.M.to_dot ~string_of_labels:(fun _ -> "") ~string_of_lits:(fun x -> Fmt.to_to_string Tn.Lits.pp x) (Tn.expand ~supply:T.State.supply s' ps)
-
+  (*Tn.M.to_dot ~string_of_labels:(fun x -> Fmt.to_to_string Tn.Lhss.pp x) ~string_of_lits:(fun x -> Fmt.to_to_string Tn.Lits.pp x) (Tn.collapse ~supply:T.State.supply @@ Tn.erase_return @@ Tn.subset ~supply:T.State.supply @@ Tn.construct ~supply:T.State.supply s' ps)*)
+  (*Tn.M.to_dot ~string_of_labels:(fun x -> Fmt.to_to_string Tn.Lhss.pp x) ~string_of_lits:(fun x -> Fmt.to_to_string Tn.Lits.pp x) (Tn.earley ~supply:T.State.supply @@ Tn.construct ~supply:T.State.supply s' ps)*)
+  Tn.M.to_dot ~string_of_labels:(fun x -> Fmt.to_to_string Tn.Lhss.pp x) ~string_of_lits:(fun x -> Fmt.to_to_string Tn.Lits.pp x) (Tn.collapse ~supply:T.State.supply @@ Tn.erase_return @@ Tn.earley ~supply:T.State.supply @@ Tn.construct ~supply:T.State.supply s' ps)
--- lib/te-bot/fixedpoint.ml
+++
@@ -1,7 +0,0 @@
-let run ~eq f start = 
-  let rec iter x = 
-    let y = f x in
-    if eq y x then x else iter y 
-  in
-  iter start
-
