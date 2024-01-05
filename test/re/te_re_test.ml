22a79b844e623a2d97ed9bba8a946a766afc2b43
Žiga Leber <mail+github@zigaleber.com>
2024-01-05 16:44:38 +0100

    Add RE test

---
+++ test/re/te_re_test.ml
@@ -0,0 +1,32 @@
+open Te_core
+module T = Types
+
+module R = Re.Abstract
+module R' = Re.Concrete(T.Codes)
+
+let test_to_seq () =
+  let num = R.lits (T.Codes.of_string "012") in
+  let dash = R.lits (T.Codes.of_string "-") in
+  let times3 = R.(num * num * num) in
+  let pn = R.(times3 * star (dash * times3 )) in
+  let r = pn in
+
+  Fmt.pr "%a" T.Codes.pp (T.Codes.of_string "0123456789");
+  Seq.iter (fun s ->
+      Fmt.pr "%a@," (T.Code.pp) s)
+    (T.Codes.to_seq @@ T.Codes.of_string "0123456789");
+
+  Fmt.pr "@[<v>";
+  Seq.iter (fun s ->
+      Fmt.pr "%a@," (Fmt.seq ~sep:(Fmt.nop) T.Code.pp) s)
+    (Seq.take 800 (R.to_seq ~cmp:T.Code.compare T.Codes.to_seq r));
+  Fmt.pr "@]";
+  Alcotest.fail "dasd"
+  (*Alcotest.check (module R') "equal" R.(lits (Ints.of_list [1; 2; 3]) * lits (Ints.of_list [1])) (R.lits (Ints.of_list [1]))*)
+
+let () =
+  Alcotest.run "re" [
+    "to_seq", [
+      Alcotest.test_case "test" `Quick test_to_seq;
+    ]
+  ]
